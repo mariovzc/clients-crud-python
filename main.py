027f6f64236e65b2459c2c72559c2cf8346a5302
Mario Vizcaino <mario.vizcainos@gmail.com>
2019-03-10 19:36:01 -0500

    list and create clients

---
+++ main.py
@@ -0,0 +1,28 @@
+
+clients = 'Mario, Alberto, '
+
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_coma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_coma():
+    global clients
+
+    clients += ', '
+
+
+if __name__ == '__main__':
+    list_clients()
+
+    create_client('Prueba')
+
+    list_clients()
