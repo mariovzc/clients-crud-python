6c5ab338d73adabc0462b498f4f98ce9c4d0428a
Mario Vizcaino <mario.vizcainos@gmail.com>
2019-03-12 22:34:34 -0500

    update clients

--- main.py
+++ main.py
@@ -10,11 +10,19 @@ def create_client(client_name):
     else:
         print('Client already in the client\'s list')
 
+
 def list_clients():
     global clients
 
     print(clients)
 
+
+def update_client(client_name, updated_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name, updated_client_name)
+    else:
+        print('Client is not in clients list')
 
 def _add_coma():
     global clients
@@ -26,20 +34,29 @@ def _print_welcome():
     print('WELCOME TO SALES')
     print('*' * 50)
     print('What would you like to do today? ')
-    print('[C]reate clients')
-    print('[D]elete clients')
+    print('[C]reate client')
+    print('[U]pdate client')
+    print('[D]elete client')
 
+def _get_client_name():
+    return input('What is the client name? ')
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name? ')
+        update_client(client_name + ',', updated_client_name + ',')
+        list_clients()
     else:
         print('Invalid Command')
